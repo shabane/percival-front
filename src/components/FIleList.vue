808b44968bad4577b767d86032364c47f48d5898
shabane <m.mohamadshabane@gmail.com>
2024-09-28 06:31:20 +0330

    feat: create component for each files

---
+++ src/components/FIleList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row user-item m-3">
+    <span class="col-1">⬇️</span>
+    <a :href="dest" class="col">{{ file_name }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FIleList",
+  props: ["file_name", "dest"],
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #3498db;
+}
+</style>
