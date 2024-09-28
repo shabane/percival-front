1aa3c9b43bd49edd3e35fe03ca4039301b4a83fb
shabane <m.mohamadshabane@gmail.com>
2024-09-28 04:49:49 +0330

    feat: make a div with its toolbox in it.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home"></div>
+  <div class="d-flex justify-content-center mt-5">
+    <div class="user-border col-9 col-lg-6">
+      <div>
+        <div class="user-toolbox">
+          <button class="btn btn-outline-secondary m-2">File</button>
+          <button class="btn btn-outline-secondary m-2">Text</button>
+          <button class="btn btn-outline-secondary m-2">Link</button>
+          <button class="btn btn-outline-secondary m-2">⚙️</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +19,18 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+.user-border {
+  border: #2ecc71 1px solid;
+  border-top: none;
+  height: 400px;
+  background-color: #373737;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+}
+
+.user-toolbox {
+  background-color: #404040;
+}
+</style>
