2a13e3e2d751544af9938692e3d51619a4742c8f
shabane <m.mohamadshabane@gmail.com>
2024-09-28 08:06:41 +0330

    feat: create two function for get a cookie and get username and password

--- src/main.js
+++ src/main.js
@@ -6,6 +6,30 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/style.css";
 
 // createApp(App).use(router).mount("#app");
+
+function getCookieValue(cookieName) {
+  const name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i].trim();
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return null;
+}
+
+function getCredits() {
+  return {
+    username: getCookieValue("username"),
+    password: getCookieValue("password"),
+  };
+}
+
 const app = createApp(App);
+app.config.globalProperties.$getCookieValue = getCookieValue;
+app.config.globalProperties.$getCredits = getCredits;
 app.use(router);
 app.mount("#app");
