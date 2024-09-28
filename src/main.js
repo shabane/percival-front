f783e19648eb8c55b2d7ca505c0b0988e40dee9e
shabane <m.mohamadshabane@gmail.com>
2024-09-28 04:48:49 +0330

    chore: installed bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).use(router).mount("#app");
+// createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
