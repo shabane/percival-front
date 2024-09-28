4907501935658357b5cebd617497dd527e8b9602
shabane <m.mohamadshabane@gmail.com>
2024-09-28 06:29:35 +0330

    feat: create a project wide stylesheet file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/style.css";
 
 // createApp(App).use(router).mount("#app");
 const app = createApp(App);
