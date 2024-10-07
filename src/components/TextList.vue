d85757ebde75e661ea9ba7de8d90fca5671ad229
shabane <m.mohamadshabane@gmail.com>
2024-10-08 02:58:46 +0330

    feat: create a TextList to show text. use swal

---
+++ src/components/TextList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row user-item m-3">
+    <span class="col-1">⬇️</span>
+    <a class="col" @click="copyToClip(text)">{{ text }}</a>
+  </div>
+</template>
+
+<script>
+import Swal from "sweetalert2";
+export default {
+  name: "TextList",
+  props: ["text"],
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    copyToClip(text) {
+      navigator.clipboard.writeText(text);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Copied!",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #3498db;
+}
+</style>
