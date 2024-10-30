b6b40fb7388aae2403e45e351d2801ca14776374
shabane <m.mohamadshabane@gmail.com>
2024-10-30 11:44:10 +0330

    feat: create a LinkView component

---
+++ src/components/LinkView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row user-item m-3">
+    <span class="col-1">⬇️</span>
+    <a class="col" :href="dest"> {{ dest }}</a>
+  </div>
+</template>
+
+<script>
+import Swal from "sweetalert2";
+export default {
+  name: "TextList",
+  props: ["dest"],
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    copyToClip(text) {
+      navigator.clipboard.writeText(text);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Copied!",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #3498db;
+}
+</style>
