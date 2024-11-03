e293821c8af916a19dd1d1a0de057cebe486fd71
shabane <m.mohamadshabane@gmail.com>
2024-11-03 17:55:31 +0330

    feat: use usernamer/passworder and add a fn to set a cookie

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./assets/style.css";
-import { isValidLink } from "@/tools";
+import { isValidLink, usernamer, passworder } from "@/tools";
 
 // createApp(App).use(router).mount("#app");
 
@@ -35,9 +35,19 @@ function getCredits() {
   }));
 }
 
+function setCookie(key, value) {
+  let date = new Date();
+  date.setTime(date.getTime() + 31 * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = key + "=" + value + ";" + expires + ";path=/";
+}
+
 const app = createApp(App);
 app.config.globalProperties.$getCookieValue = getCookieValue;
 app.config.globalProperties.$getCredits = getCredits;
 app.config.globalProperties.$isValidLink = isValidLink;
+app.config.globalProperties.$usernamer = usernamer;
+app.config.globalProperties.$passworder = passworder;
+app.config.globalProperties.$setCookie = setCookie;
 app.use(router);
 app.mount("#app");
