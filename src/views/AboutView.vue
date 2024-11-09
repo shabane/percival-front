3ee2887807db48acfbbbc96d851a4d8ec816ee16
shabane <m.mohamadshabane@gmail.com>
2024-11-09 20:54:58 +0330

    feat: write about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>
+      <span style="color: #5d6d7e">P</span
+      ><span style="color: #85929e">ercival</span>
+    </h2>
+
+    <p>
+      It`s just a simple file sharing web site. not just files, also texts and
+      links.<br />
+      you can send or receive just via your <i>username</i> that the site will
+      gave you
+      <span style="color: #4a5765">r</span>
+      <span style="color: #85929e">a</span>
+      <span style="color: #5d6d7e">n</span>
+      <span style="color: #85929e">d</span>
+      <span style="color: #5d6d7e">o</span>
+      <span style="color: #85929e">m</span>
+      <span style="color: #5d6d7e">l</span>
+      <span style="color: #85929e">y</span>.
+    </p>
+    <hr style="width: 50%; margin-left: 25%" />
+    <span style="color: #85929e">></span>
+    <a href="https://github.com/shabane/percival"> Github </a>
+    <br />
+    <span style="color: #85929e">></span>
+    <a href="https://twitter.com/m_shabane"> Twitter </a>
   </div>
 </template>
+
+<style scoped>
+.about {
+  margin-top: 1%;
+}
+a {
+  text-decoration: none;
+  color: #4a5765;
+}
+a:hover {
+  color: #85929e;
+}
+</style>
